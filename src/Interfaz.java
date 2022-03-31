91d234bbf3e4cffee1ab9d66c6d0ca7e0c606f61
JPampers <jprodriguezcano99@gmail.com>
2022-03-30 18:13:18 -0600

    Interfaz basica

---
+++ src/Interfaz.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Interfaz {
+    private JPanel panel1;
+    private JLabel Card1;
+    private JLabel Card2;
+    private JLabel Card3;
+    private JLabel Card4;
+    private JLabel Card5;
+    private JLabel Card6;
+    private JLabel Card8;
+    private JLabel Card9;
+    private JLabel Card7;
+    private JLabel Card12;
+    private JLabel Card11;
+    private JLabel Card10;
+}
